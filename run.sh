2c06da7218ad4f6cfd3d88ecb2c069b110726d80
Ignacio Bonelli <ignacio@nachodigital.com.ar>
2022-06-14 00:51:50 -0300

    Improving the simple run script.

--- run.sh
+++ run.sh
@@ -1,84 +1,24 @@
 PYRUN="main.py"
 #PYRUN="main_astar.py"
+DIR="latest_run"
 
-FILE="world01"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
+function run_world {
+	echo "Running "${FILE}" ================"
+	SAVE=${DIR}"/"${FILE}"/"
+	python3 ${PYRUN} ${FILE}.csv
+	mv navigation.log ${FILE}_navigation.log
+	mkdir -p ${SAVE}
+	mv ${FILE}_* ${SAVE}
+}
 
-FILE="world02"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
+while read WFNAME; do
+	FILE="$WFNAME"
+	run_world
+done <worlds_to_run.txt
 
-FILE="world03"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
-
-FILE="world04"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
-
-FILE="world11"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
-
-FILE="world12"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
-
-FILE="world13"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
-
-FILE="world14"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
-
-FILE="world15"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
-
-FILE="world21"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
-
-FILE="world22"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
-
-FILE="world23"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
-
-FILE="world24"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
-
-FILE="world31"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
-
-FILE="world32"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
-
-FILE="world33"
-echo "Running "${FILE}" ================"
-python3 ${PYRUN} ${FILE}.csv
-mv navigation.log ${FILE}_navigation.log
+while read WFNAME; do
+	FILE="$WFNAME"
+	run_world
+done <worlds_to_run2.txt
 
 cat *.txt > All_Runs.txt
